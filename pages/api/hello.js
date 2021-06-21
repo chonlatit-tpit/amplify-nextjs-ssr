08870ade07537879fb58073861194c957f32b0bf
Chonlatit Wetchasilp <chonlatit_w@tripetch-it.co.th>
2021-06-21 08:33:03 +0700

    [added] - amplify configure

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import Amplify, { withSSRContext } from "aws-amplify";
+import config from "../../src/aws-exports.js";
+
+// Amplify SSR configuration needs to be done within each API route
+Amplify.configure({ ...config, ssr: true });
+
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
