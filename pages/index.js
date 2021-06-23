0204dc292fce4f52e465821a34ad00c46833aa47
Chonlatit Wetchasilp <chonlatit_w@tripetch-it.co.th>
2021-06-23 14:02:20 +0700

    [test] - lambda@edge

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>SSR</h1>
+      <h1>SSR [2021-06-23 | 14:01]</h1>
     </div>
   )
 }
